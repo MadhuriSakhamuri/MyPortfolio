fc21845d5f94e602586486b1089c11b41911bf97
MadhuriSakhamuri <madhuri.sakhamuri99@gmail.com>
2024-07-04 16:51:43 -0400

    Create generate-static-files.sh

---
+++ MadhuriPortfolio/generate-static-files.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+# Ensure output directory exists
+OUTPUT_DIR="out/static"
+mkdir -p $OUTPUT_DIR
+
+# Copy static files (HTML, CSS, JS) to output directory
+cp -r wwwroot/* $OUTPUT_DIR/
+cp -r Views/* $OUTPUT_DIR/
