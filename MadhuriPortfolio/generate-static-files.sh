0c5acf679484d991fe1545851060acd6cf5dc75a
MadhuriSakhamuri <madhuri.sakhamuri99@gmail.com>
2024-07-04 17:09:07 -0400

    Commit changes

--- MadhuriPortfolio/generate-static-files.sh
+++ MadhuriPortfolio/generate-static-files.sh
@@ -5,5 +5,5 @@ OUTPUT_DIR="out/static"
 mkdir -p $OUTPUT_DIR
 
 # Copy static files (HTML, CSS, JS) to output directory
-cp -r wwwroot/* $OUTPUT_DIR/
-cp -r Views/* $OUTPUT_DIR/
+cp -r ./MadhuriPortfolio/wwwroot/* $OUTPUT_DIR/
+cp -r ./MadhuriPortfolio/Views/* $OUTPUT_DIR/
